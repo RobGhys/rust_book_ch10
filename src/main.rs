c60b4447f60f73c38d741d1634a8b359f5fc6f71
rg63 <rg@kando.care>
2025-03-24 11:35:07 +0100

    first

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 // params: list -> a reference to a slice of i32 values
 fn largest_i32(list: &[i32]) -> &i32 {
     // Get a pointer to the 0-th value of 'list'
@@ -67,6 +68,60 @@ impl<T, U> Point<T, U> {
     }
 }
 
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }    
+}
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news. {}", item.summarize());
+}
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    }
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn main() {
     let nb_list = vec![32, 2, 3, 69];
     let result = largest_i32(&nb_list);
@@ -91,4 +146,25 @@ fn main() {
     let char_point = Point { x: "Coucou", y: "Byebye" };
     let p_mixed = int_and_float.mixup(char_point);
     println!("This is magic >>> {}, {}", p_mixed.x, p_mixed.y);
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
+    returns_summarizable();
+
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {result}");
+    }
 }
